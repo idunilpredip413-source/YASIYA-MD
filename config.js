34d2796960d0d24cecff049deca37ab5a2338017
idunilpredip413-source <idunilpredip413@gmail.com>
2025-10-09 16:23:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~UV5VERiR#gxoZ89iJcMGk-DYogOWexuqO8iUM6D_VknWQ3kIkkMw'
 };
